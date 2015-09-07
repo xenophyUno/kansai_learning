1e30c0199aa63ccd3912d4c72e340b9121ee95b7
mizuno <moe.mizuno.xenophy@gmail.com>
2015-09-07 13:27:09 +0900

    no message

---
+++ mjstest/imgdisp.js
@@ -0,0 +1,13 @@
+
+
+function disp(){
+	var e = document.getElementById("canvas");
+	e.style.background = "url('img/logo.gif')";
+}
+
+
+
+function disp2(){
+	var a = document.getElementById("img");
+	a.src = "url('img/logo.gif')";
+}
---
+++ mjstest/tbltest.js
@@ -0,0 +1,11 @@
+
+// 表　点数計算表１
+function calc(){
+	t1=parseInt(document.getElementById("tensu1").value);
+	t2=parseInt(document.getElementById("tensu2").value);
+	t3=parseInt(document.getElementById("tensu3").value);
+	document.getElementById("sumcell").value = t1 + t2 + t3;
+}
+
+//　表点数計算表２
+
