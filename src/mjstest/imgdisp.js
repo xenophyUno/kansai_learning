0c84f1c93c5d42a7bb430ea22fb29f0ab76a32f4
mizuno <moe.mizuno.xenophy@gmail.com>
2015-09-07 18:31:17 +0900

    no message

--- src/mjstest/imgdisp.js
+++ src/mjstest/imgdisp.js
@@ -1,3 +1,25 @@
+
+
 function disp(){
-	
+	var e = document.getElementById("canvas");
+	e.style.background = "url('img/logo.gif')";
 }
+
+
+
+ function disp2(){
+	document.createElement('img');
+	var image = new Image();
+	var i = document.getElementById("imgdiv");
+	i.appendChild(image);
+	image.onload = function(e){
+		//var image_size= ImageGetNaturalSize(image);
+	};
+	image.src = "img/logo.gif";
+}
+
+
+/*function disp2(){
+ var Img2 = document.getElementById('img2');
+ Img2.src = 'img/logo.gif';
+}*/
